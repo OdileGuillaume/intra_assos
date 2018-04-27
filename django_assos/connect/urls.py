6ee855bc0de7af4871116d275904856ee575a1ff
Erwan Chabaud <chabau_ed@epita.fr>
2018-04-27 21:35:28 +0200

    DJANGO: created pages for the 'soutenance'

--- django_assos/connect/urls.py
+++ django_assos/connect/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('accueil', views.accueil, name='accueil'),
+    path('demande_reunion', views.demande_reunion, name='demande_reunion'),
+    path('index', views.index, name='index'),
+    path('', views.accueil, name='accueil'),
 ]
--- django_assos/connect/views.py
+++ django_assos/connect/views.py
@@ -2,17 +2,21 @@
 from django.shortcuts import render
 
 
+def demande_reunion(request):
+    return render(
+        request,
+        'demande_reunion.html',
+    )
+
+
+def accueil(request):
+    return render(
+        request,
+        'accueil.html',
+    )
+
+
 def index(request):
-
-    """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
-    """
-    return HttpResponse(
-
-        <h1>Bienvenue sur mon blog !</h1>
-
-        <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>
-
-    )"""
     return render(
         request,
         'index.html',
